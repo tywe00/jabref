e1e5d2d693b16d2a4e711d8665b411acc5deb451
Alexander Widman <119602587+AlexWidman@users.noreply.github.com>
2024-03-05 22:16:52 +0100

    test: progress on test for tooltip hover
    - Intended to test String returned from tooltip preview when hovering.
    - Currently not working properly due to lack of in-data.

--- src/test/java/org/jabref/preferences/PreferencesEntryPreviewTest.java
+++ src/test/java/org/jabref/preferences/PreferencesEntryPreviewTest.java
@@ -1,26 +1,42 @@
 package org.jabref.preferences;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.stage.Stage;
+
+import org.jabref.gui.maintable.BibEntryTableViewModel;
+import org.jabref.model.database.BibDatabaseContext;
+import org.jabref.model.entry.BibEntry;
+import org.jabref.model.entry.field.StandardField;
 
 import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
 import static org.testfx.api.FxAssert.verifyThat;
 
 public class PreferencesEntryPreviewTest extends ApplicationTest {
+    private PreviewPreferences previewPreferences;
+    private BibDatabaseContext database;
+    private final PreferencesService preferencesService = mock(PreferencesService.class);
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/java/org/jabref/gui/preferences/preview/PreviewTab.fxml"));
-        Parent root = loader.load();
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
+        previewPreferences = preferencesService.getPreviewPreferences();
         stage.show();
+    }
+
+    @Test
+    public void testHoverPreviewContent() {
+        BibEntry entry = new BibEntry(BibEntry.DEFAULT_TYPE)
+                .withField(StandardField.ABSTRACT, "test abstract")
+                .withField(StandardField.AUTHOR, "author")
+                .withField(StandardField.DATE, "2001");
+
+        BibEntryTableViewModel.showTooltipProperty().set(true);
+        String preview = entry.getBibPreviewTooltip(previewPreferences, database);
+        assertEquals(preview, "hello");
     }
 
     @Test
